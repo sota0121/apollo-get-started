1b8f219e742f7a9dccdc5d631bf4b60965564f0a
masuda.sota <masuda.sota@moneyforward.co.jp>
2022-12-13 16:39:21 +0900

    add step3 codes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Apollo Client Tutorials
+
+## References
+
+https://www.apollographql.com/docs/react/get-started/
+
+
+## Step 3 result
+
+![Image of Step 3 result](/img/step3-result.png)
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
